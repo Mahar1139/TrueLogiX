6836c845c036ec98081e0d04c711e413362e225d
Ashish Mahar <asmaharg1139@gmail.com>
2025-06-26 15:05:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,11 +37,10 @@ export default function Home() {
                 </div>
               </div>
               <Image
-                src="https://placehold.co/600x400.png"
+                src="/truelogix.jpg"
                 width="600"
                 height="400"
-                alt="Hero"
-                data-ai-hint="programming learning"
+                alt="TrueLogiX Hero"
                 className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last"
               />
             </div>
